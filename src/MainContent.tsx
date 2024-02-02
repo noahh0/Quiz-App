d639d4705428ef98580ef597c849795a5487b0a7
Noah Huck <119907714+noahh0@users.noreply.github.com>
2024-02-02 00:00:29 -0800

    setup site scaffold

---
+++ src/MainContent.tsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main>
+      <ul>
+        <li>content</li>
+        <li>more content</li>
+        <li>look at all this content</li>
+      </ul>
+    </main>
+  );
+}
+
+export default MainContent;
